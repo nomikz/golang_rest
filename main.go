ead1228f4285ccdb224f6dd674a5097e5aeea5dd
Berarysuly Namazbek <n.bekarysuly@tha.kz>
2021-01-24 14:40:11 +0600

    minor changes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
 		fmt.Println("someone came in again")
 		_, _ = fmt.Fprintf(w, "<h1>Hello kitty</h1>")
 	})
